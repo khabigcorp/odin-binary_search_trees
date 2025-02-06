8349b072ed2cf9fe7aa083bb68cc4ae38baba8d1
khabigcorp <99237998+khabigcorp@users.noreply.github.com>
2025-02-06 19:41:30 +0800

    Finished project

--- main.rb
+++ main.rb
@@ -1,36 +1,25 @@
 require './lib/Tree.rb'
 
-new_array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-new_tree = Tree.new
-new_tree.build_tree(new_array)
-new_tree.pretty_print
-new_tree.insert(0)
-new_tree.insert(-1)
-new_tree.insert(-2)
-new_tree.insert(-3)
-new_tree.pretty_print
-
-new_tree.delete(8)
-new_tree.pretty_print
-
-found_node = new_tree.find(4)
-puts found_node.left.data
-puts found_node.right.data
-
-new_tree.level_order {|node| print "#{node.data} "}
+new_array = (Array.new(15) { rand(1..100) })
+tree = Tree.new
+tree.build_tree(new_array)
+puts tree.balanced?
+tree.preorder {|node| print "#{node.data} "}
 puts
-new_tree.inorder {|node| print "#{node.data} "}
+tree.postorder {|node| print "#{node.data} "}
 puts
-new_tree.preorder {|node| print "#{node.data} "}
+tree.inorder {|node| print "#{node.data} "}
 puts
-new_tree.postorder {|node| print "#{node.data} "}
+tree.insert(150)
+tree.insert(200)
+tree.insert(2391)
+puts tree.balanced?
+tree.rebalance
+puts tree.balanced?
+tree.preorder {|node| print "#{node.data} "}
 puts
-
-puts "Height"
-puts new_tree.height(new_tree.find(4))
-puts "Depth"
-puts new_tree.depth(new_tree.find(4))
-puts "Balanced?"
-puts new_tree.balanced?
-new_tree.rebalance
-new_tree.pretty_print
+tree.postorder {|node| print "#{node.data} "}
+puts
+tree.inorder {|node| print "#{node.data} "}
+puts
+tree.pretty_print
---
+++ pre_test.rb
@@ -0,0 +1,36 @@
+require './lib/Tree.rb'
+
+new_array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+new_tree = Tree.new
+new_tree.build_tree(new_array)
+new_tree.pretty_print
+new_tree.insert(0)
+new_tree.insert(-1)
+new_tree.insert(-2)
+new_tree.insert(-3)
+new_tree.pretty_print
+
+new_tree.delete(8)
+new_tree.pretty_print
+
+found_node = new_tree.find(4)
+puts found_node.left.data
+puts found_node.right.data
+
+new_tree.level_order {|node| print "#{node.data} "}
+puts
+new_tree.inorder {|node| print "#{node.data} "}
+puts
+new_tree.preorder {|node| print "#{node.data} "}
+puts
+new_tree.postorder {|node| print "#{node.data} "}
+puts
+
+puts "Height"
+puts new_tree.height(new_tree.find(4))
+puts "Depth"
+puts new_tree.depth(new_tree.find(4))
+puts "Balanced?"
+puts new_tree.balanced?
+new_tree.rebalance
+new_tree.pretty_print
